f843487dbfdc85a72afa90a9620c8751952f7756
Kurosawa-rubyrubyru <52451462+Kurosawa-rubyrubyru@users.noreply.github.com>
2022-09-01 00:28:27 +0800

    Update Percolation.java

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -93,7 +93,7 @@ public class Percolation {
 
     // does the system percolate?
     public boolean percolates() {
-        if (whetherisfull = false) {
+        if (whetherisfull == false) {
             for (int i = 0; i < N; i += 1) {
                 if (isFull(N - 1, i)) {
                     return true;
