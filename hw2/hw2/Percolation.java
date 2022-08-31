008f56772ce69c0e6b178213bf9fb3c4b7d265f3
Kurosawa-rubyrubyru <52451462+Kurosawa-rubyrubyru@users.noreply.github.com>
2022-09-01 00:49:11 +0800

    Update Percolation.java

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -8,13 +8,9 @@ public class Percolation {
 
     private boolean[][] openclosesystem;
 
-    private boolean[][] fullsystem;
-
     private int N;
 
     private WeightedQuickUnionUF union;
-
-    private boolean whetherisfull;
 
 //    private int[] unionToArray(int id) {
 //        int[] ans = new int[2];
@@ -38,17 +34,9 @@ public class Percolation {
             }
         }
 
-        fullsystem = new boolean[N][N];
-        for (int i = 0; i < N; i += 1) {
-            for (int j = 0; j < N; j += 1) {
-                fullsystem[i][j] = false;
-            }
-        }
-
         union = new WeightedQuickUnionUF(1 + N * N);
         sumopen = 0;
         this.N = N;
-        whetherisfull = false;
     }
 
     // open the site (row, col) if it is not open already
@@ -81,9 +69,7 @@ public class Percolation {
 
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
-        fullsystem[row][col] = union.connected(0, arrayToUnion(row, col)) && isOpen(row, col);
-        whetherisfull = true;
-        return (fullsystem[row][col]);
+        return (union.connected(0, arrayToUnion(row, col)) && isOpen(row, col));
     }
 
     // number of open sites
@@ -93,21 +79,13 @@ public class Percolation {
 
     // does the system percolate?
     public boolean percolates() {
-        if (whetherisfull == false) {
-            for (int i = 0; i < N; i += 1) {
-                if (isFull(N - 1, i)) {
-                    return true;
-                }
+        for (int i = 0; i < N; i += 1) {
+            if (isFull(N - 1, i)) {
+                return true;
             }
-            return false;
-        } else {
-            for (int i = 0; i < N; i += 1) {
-                if (fullsystem[N - 1][i]) {
-                    return true;
-                }
-            }
-            return false;
+
         }
+        return false;
     }
 
 
