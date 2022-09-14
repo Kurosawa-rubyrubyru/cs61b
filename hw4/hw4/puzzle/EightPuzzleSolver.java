3d006513b27265347b2686bccdb8c5c71c0782af
Kurosawa-rubyrubyru <52451462+Kurosawa-rubyrubyru@users.noreply.github.com>
2022-09-14 13:09:57 +0800

    Update EightPuzzleSolver.java

--- hw4/hw4/puzzle/EightPuzzleSolver.java
+++ hw4/hw4/puzzle/EightPuzzleSolver.java
@@ -1,8 +1,5 @@
 package hw4.puzzle;
 
-import edu.princeton.cs.algs4.In;
-import edu.princeton.cs.algs4.StdOut;
-
 public class EightPuzzleSolver {
     /***********************************************************************
      * Test routine for your Solver class. Uncomment and run to test
